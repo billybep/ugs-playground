57d712eea7a0d0712a8f16ba8daea3ed18f4648a
Billy Pesoth <40064669+billybep@users.noreply.github.com>
2022-12-08 17:49:11 +0800

    add economy scene for playground Store + add authenticate guest player (#1)

---
+++ Assets/dev-billy/Scripts/EconomyMabager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EconomyMabager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Button
+    public void OnClickBtnBack()
+    { 
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+}
